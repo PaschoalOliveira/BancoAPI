8c1c673145321afe3c38ddb758e6bd81a4834724
Paschoal <paschoaloliveira@hotmail.com>
2021-08-10 10:26:32 -0300

    Commit da estrutura de utilização de Json Web Token

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,22 +1,49 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.demo.config.security.JwtAuthFilter;
+import com.example.demo.config.security.JwtService;
+import com.example.demo.service.UsuarioServiceApi;
 
 @EnableWebSecurity
+//Classe responsável por definir questões de autenticação e autorização
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	UsuarioServiceApi usuarioServiceApi;
+	
+	@Autowired
+	JwtService jwtService;
+	
 	@Bean
+	//Encoder responsável por codificar a senha
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
-	//Autenticação
+	@Bean
+	//Filtro responsável por definir o processo de autenticação com JsonWebToken
+	public OncePerRequestFilter jwtFilter() {
+		return new JwtAuthFilter(jwtService, usuarioServiceApi);
+	}
+	
+	@Autowired
+	//Classe responsável por buscar as informações de usuário no banco. Classe implementada por nós
+	private UsuarioServiceApi usuarioService;
+	
+	//Autenticação Antiga em memória
+	/*
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
@@ -26,14 +53,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.password(passwordEncoder().encode("123"))
 		.roles("USER");
 	}
+	*/
+	
+	//Autenticação ocorre com dados do banco
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//Define a classe de serviço responsável por buscar as informações no banco
+		auth.userDetailsService(usuarioService)
+		.passwordEncoder(passwordEncoder());
+	}
 	
 	//Autorização
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		http.csrf().disable()
+		// Forma  ser feita sem jwt
+		/*
+		http.cors().and()
+		.csrf().disable()
 		.authorizeRequests()
-		.antMatchers("/v1/clientes/**").hasRole("USER");
+		.antMatchers("/v1/**").hasRole("USUARIO")
+		.and()
+		.httpBasic();
+		*/
+		//Forma de se autenticar usando o JsonWebToken
+		http.cors().and()
+		.csrf().disable()
+		.authorizeRequests()
+		.antMatchers("/v1/**").hasRole("USUARIO")
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
+		
 	}
 	
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO cliente(`nome`,`cpf`,`telefone`,`sexo`)
+VALUES('Rafael Contreiras',123457,7132450987,'M');
+-- INSERT
+INSERT INTO cliente(`nome`,`cpf`,`telefone`,`sexo`)
+VALUES('Rafael',98723,71324,'M');
